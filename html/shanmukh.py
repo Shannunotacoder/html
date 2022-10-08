827726074054f57a92ad0bd9a5de95b3a1ae6d6f
Shannunotacoder <109965164+Shannunotacoder@users.noreply.github.com>
2022-10-08 13:07:14 +0530

    Add files via upload

---
+++ html/shanmukh.py
@@ -0,0 +1,49 @@
+# types of typecasting....
+# basically there are two types of typecasting..
+# implict:data typecasts according to the rank.
+
+# a = 4
+# b = 5.4
+# print(a+b)
+
+# explicit:we have to typecast the data..(if possible..)
+# a=4
+# b=float(a)
+# print(b)
+
+# if elif and else statements...\
+# my_name = input("Enter Your Name:\n")
+# list = ["Shanmukh", "Datta", "Aashish", "Kartik"]
+# if my_name in list:
+#     print("You are Welcome")
+# elif my_name == "Kunal" :
+#     print("You are also welcome")
+# else:
+# print("get out")
+
+
+# a = int(input("Enter 1st number:\n"))
+# b = int(input("Enter 2nd number:\n"))
+# c = int(input("Enter 3rd number:\n"))
+# d = int(input("Enter 4th number:\n"))
+# if a > b:
+#     f1 = a
+# else:
+#     f1 = b
+# if c > d:
+#     f2 = c
+# else:
+#     f2 = d
+# if f1  > f2:
+#     print(str(f1)+" is the greatest")
+# else:
+#     print(str(f2)+" is the greatest")
+
+
+
+
+        
+
+    
+
+
